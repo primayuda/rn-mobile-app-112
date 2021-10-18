45129062827a44b3dc5fc284b262c3823626498d
Primayuda <aprita.primayuda@gmail.com>
2021-10-18 21:35:27 +0700

    Working Google Map

--- components/UserMap.js
+++ components/UserMap.js
@@ -1,29 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Image } from 'react-native';
+import { Platform, Text, View, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const UserMap = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS === 'android' && !Constants.isDevice) {
+        setErrorMsg(
+          'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
+        );
+        return;
+      }
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  let latitude = -6.5761009;
+  let longitude = 106.8095802250028;
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    latitude = location.coords.latitude;
+    longitude = location.coords.longitude;
+    console.log(latitude);
+    console.log(longitude);
+  }
   return (
     <View style={styles.container}>
-      {/* <Image source={require('../assets/MockupLight.png')} style={styles.imageContainer} /> */}
+
       <MapView
           style={styles.mapContainer}
           provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: -6.576082664311965,
-            longitude: 106.81006149978741,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.01,
-          }}
+          // initialRegion={{
+          //   latitude: -6.5761009,
+          //   longitude: 106.8095802250028,
+          //   latitudeDelta: 0.015,
+          //   longitudeDelta: 0.01,
+          // }}
           region={{
-            latitude: -6.576082664311965,
-            longitude: 106.81006149978741,
+            latitude: latitude,
+            longitude: longitude,
             latitudeDelta: 0.015,
             longitudeDelta: 0.01,
           }}
-          showsUserLocation
+          showsUserLocation={true}
         />
     </View>
   )
